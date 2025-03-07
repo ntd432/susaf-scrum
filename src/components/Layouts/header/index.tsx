26fbb9f460c00a565bff6aacb640fc8109015e5c
Tasluf665 <tasluf665@gmail.com>
2025-03-07 14:44:23 +0200

    Deleting extra tab and pages

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -52,13 +52,6 @@ export function Header() {
           <SearchIcon className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 max-[1015px]:size-5" />
         </div> */}
 
-        <ThemeToggleSwitch />
-
-        <Notification />
-
-        <div className="shrink-0">
-          <UserInfo />
-        </div>
       </div>
     </header>
   );
--- src/components/Layouts/sidebar/data/index.ts
+++ src/components/Layouts/sidebar/data/index.ts
@@ -30,27 +30,6 @@ export const NAV_DATA = [
         ],
       },
       {
-        title: "Forms",
-        icon: Icons.Alphabet,
-        items: [
-          {
-            title: "Form Elements",
-            url: "/forms/form-elements",
-          },
-          {
-            title: "Form Layout",
-            url: "/forms/form-layout",
-          },
-        ],
-      },
-
-      {
-        title: "Retrospective",
-        icon: Icons.ChevronUp,
-        url: "/retrospective",
-        items: [],
-      },
-      {
         title: "Sustainability Hub",
         url: "/sustainability",
         icon: Icons.Alphabet,
@@ -62,33 +41,6 @@ export const NAV_DATA = [
           {
             title: "Sustainability Knowledge Update",
             url: "/sustainability/knowledge",
-          },
-      {
-            title: "FAQ",
-            url: "/faq/new",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: "OTHERS",
-    items: [
-      {
-        title: "Knowledge Hub",
-        icon: Icons.PieChart,
-        items: [
-          {
-            title: "User Manual",
-            url: "/ui-elements/alerts",
-          },
-          {
-            title: "FAQs",
-            url: "/ui-elements/buttons",
-          },
-          {
-            title: "Resources",
-            url: "/ui-elements/buttons",
           },
         ],
       },
